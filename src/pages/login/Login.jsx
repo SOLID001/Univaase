586c0af7d9c7a7830382b03852e1bc84b486316a
Lawal <lawalsamuel31@gmail.com>
2024-09-16 14:28:28 -0700

    my first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
+
+const Login = () => {
+  const { login } = useContext(AuthContext);
+
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hi There.</h1>
+          <p>
+          It's great to see you again! 
+                    We’re excited to continue this journey with you as you connect, 
+                    share, and grow with your fellow university students. 
+                    Dive back in to see what's new, catch up with your friends, 
+                    and join in on the latest discussions.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Uni
+            vaase.</h1>
+          <p>
+          It's great to see you again! 
+                    We’re excited to continue this journey with you as you connect, 
+                    share, and grow with your fellow university students. 
+                    Dive back in to see what's new, catch up with your friends, 
+                    and join in on the latest discussions.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+          <button>Login</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
